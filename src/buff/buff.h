055fbf40cf392e38047b7d4f931e5e1c43601a8b
FlitzyBlue332 <16520412@std.stei.itb.ac.id>
2021-11-20 11:07:17 +0700

    added buff(temp)

---
+++ src/buff/buff.h
@@ -0,0 +1,23 @@
+/*  modul tabel skill
+    Berisi definisi dan semua primitif pemrosesan list skill
+    List skill dalam bentuk array
+    Penempatan elemen selalu rapat kiri
+*/
+
+#include "boolean.h"
+
+#ifndef BUFF_H
+#define BUFF_H
+
+typedef struct {
+    boolean info [4]
+} Buff;
+/*
+#define ImunTel(b) b.info[0]
+#define CerGan(b) b.info[1]
+#define BesarHoki(b) b.info[2]
+#define KecilHoki(b) b.info[3]
+*/
+
+
+#endif // BUFF_H
--- src/skill/Skill/Skill.c
+++ src/skill/Skill/Skill.c
@@ -752,6 +752,20 @@ void mesinPenukarPosisi(List *skillPemain, address p)
 }
 
 
+// dari buff.c -----------------------------------------------------------------------------------------------
+void CreateBuff(Buff *buff)
+{
+    for(int i=0; i< 4; i++){
+        (*buff).info[i] = false;
+    }
+}
+
+void setAfterTurn(Buff *buff)
+{
+    (*buff).info[1]= false;
+    (*buff).info[2]= false;
+    (*buff).info[3]= false;
+}
 
 
 // Masukin ke inisialisasi permainan -------------------------------------------
