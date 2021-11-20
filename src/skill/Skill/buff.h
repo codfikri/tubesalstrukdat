fa74659cd28b5b3b9722179fc0b5478bdf507a09
FlitzyBlue332 <16520412@std.stei.itb.ac.id>
2021-11-20 10:35:56 +0700

    nambah buff.h

---
+++ src/skill/Skill/buff.h
@@ -0,0 +1,15 @@
+/*  modul tabel skill
+    Berisi definisi dan semua primitif pemrosesan list skill
+    List skill dalam bentuk array
+    Penempatan elemen selalu rapat kiri
+*/
+
+#include "boolean.h"
+
+#ifndef BUFF_H
+#define BUFF_H
+
+typedef Buff boolean[4];
+
+
+#endif // BUFF_H
